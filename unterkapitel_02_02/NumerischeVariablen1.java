9ac23d712d09f73e0971ebd704acf036748f0039
bacaiib <roman-frommhold@gmx.net>
2024-01-28 20:55:53 +0100

    NumerischeVariablen1

---
+++ unterkapitel_02_02/NumerischeVariablen1.java
@@ -0,0 +1,18 @@
+package unterkapitel_02_02;
+
+public class NumerischeVariablen1 {
+
+    public static void main(String[] args) {
+        
+    byte ältesterMensch                = 120;
+    short deutschlandundAustralieninKM = 16000;
+    int erdeSonneKm                    = 149600000;
+    long lichtJahrinKM                 = 9500000000000L;
+    
+    System.out.println("Der älteste Mensch der Welt ist "+ältesterMensch+" Jahre alt!");
+    System.out.println("Zwischen Deutschland und Australien liegen "+deutschlandundAustralieninKM+" KM");
+    System.out.println("Von der Erde bis zur Sonne sind es "+erdeSonneKm+" KM");
+    System.out.println("Ein Licht Jahr misst "+lichtJahrinKM+" KM");
+
+    }
+}
