44d8b2f91a06e0bb4ea688b10b406b0f45ea3e71
bacaiib <roman-frommhold@gmx.net>
2024-02-13 21:49:34 +0100

    breakDemo+

---
+++ unterkapitel_02_05/ContinueDemo.java
@@ -0,0 +1,15 @@
+package unterkapitel_02_05;
+
+public class ContinueDemo {
+
+    public static void main(String[] args) {
+        
+        for (int i = 1; i < 100; i++){
+            if (i % 13 == 0){ System.out.println(i + " ist durch 13 Teilbar");
+                // continue;
+            }
+            
+        }
+    }
+
+}
