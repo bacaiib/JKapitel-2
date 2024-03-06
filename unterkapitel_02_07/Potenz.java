77fb618db8e89e1f35b5add8e193411a74c7d79a
bacaiib <roman-frommhold@gmx.net>
2024-03-06 15:06:01 +0100

    potenz

---
+++ unterkapitel_02_07/Potenz.java
@@ -0,0 +1,21 @@
+package unterkapitel_02_07;
+
+public class Potenz {
+
+    public static void main(String[] args) {
+        
+     
+        System.out.println(potenzB(2, 4));
+
+    }
+
+    public static int potenzB(int n1, int n2){
+        int j = n1;
+        while (n2>1){
+           n1*=j;
+           n2--;
+        }
+        return n1;
+    }
+
+}
